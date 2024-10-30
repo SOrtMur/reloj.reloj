6275138ebf94f5ffd6610416d414f5fa5a4c7873
Salvador Ortega Murcia <167804678+SOrtMur@users.noreply.github.com>
2024-10-30 19:04:45 +0100

    Create insertar.php

---
+++ insertar.php
@@ -0,0 +1,22 @@
+<?php 
+  include_once "../config.php";
+	
+  if (isset($_POST['Insertar'])){
+		$conex = new mysqli($host,$user,$password,$db);
+		if(isset($_POST[''])){
+			$apellido1 = $_POST['Apellido1']; 
+		}
+		if(isset($_POST['Apellido2']) && filter_var($_POST['Apellido2'],FILTER_SANITIZE_STRING)){
+			$apellido2 = $_POST['Apellido2']; 
+		}
+  $consulta = "insert into alumnos values ('$nombre','$apellido1','$apellido2','$dni');";
+  
+  if($conex->query($consulta)){
+			echo "Insertado correctamente.";
+		} else {
+			echo "Error en la inserción.<br>";
+			echo $consulta;
+			echo $conex->error;
+		}
+  $conex->close();
+?>
