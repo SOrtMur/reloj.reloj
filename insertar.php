bc9162606a716921c06923195c073d358107076b
Salvador Ortega Murcia <167804678+SOrtMur@users.noreply.github.com>
2024-10-30 19:06:54 +0100

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -2,21 +2,21 @@
   include_once "../config.php";
 	
   if (isset($_POST['Enviar'])){
-		$conex = new mysqli($host,$user,$password,$db);
-		if(isset($_POST[''])){
-			$apellido1 = $_POST['Apellido1']; 
-		}
-		if(isset($_POST['Apellido2']) && filter_var($_POST['Apellido2'],FILTER_SANITIZE_STRING)){
-			$apellido2 = $_POST['Apellido2']; 
-		}
-  $consulta = "insert into alumnos values ('$nombre','$apellido1','$apellido2','$dni');";
+	$conex = new mysqli($host,$user,$password,$db);
+	if(isset($_POST['nombre'])){
+		$apellido1 = $_POST['nombre']; 
+	}
+	if(isset($_POST['edad'])){
+		$apellido2 = $_POST['edad']; 
+	}
+  $consulta = "insert into alumnos values ('$nombre','$edad');";
   
   if($conex->query($consulta)){
-			echo "Insertado correctamente.";
-		} else {
-			echo "Error en la inserción.<br>";
-			echo $consulta;
-			echo $conex->error;
-		}
+	echo "Insertado correctamente.";
+} else {
+	echo "Error en la inserción.<br>";
+	echo $consulta;
+	echo $conex->error;
+}
   $conex->close();
 ?>
